0a2983251644cad08eba4efeae3d6a86d0ad83f6
xfangcosmo <fang.307@osu.edu>
2019-04-27 16:59:21 -0700

    test RSD

--- theory/cosmo2D_exact_fft.c
+++ theory/cosmo2D_exact_fft.c
@@ -166,10 +166,28 @@ void f_chi_for_Psi_cl(double* chi_ar, int Nchi, double* f_chi_ar, int ni){
 	}
 }
 
+///////
+void f_chi_for_Psi_cl_RSD(double* chi_ar, int Nchi, double* f_chi_RSD_ar, int ni){
+	double g0 =1./growfac(1.);
+	double a, z;
+	int i;
+	double real_coverH0 = cosmology.coverH0 / cosmology.h0;
+	double pf;
+	for(i=0;i<Nchi;i++) {
+		a = a_chi(chi_ar[i] / real_coverH0) ; // first convert unit of chi from Mpc to c/H0
+		z = 1./a - 1.;
+		pf = (pf_photoz(z,ni)<0.)? 0:pf_photoz(z,ni); // get rid of unphysical negatives
+		// pf =pf_photoz(z,ni);
+		f_chi_RSD_ar[i] = -chi_ar[i] * pf*growfac(a)*g0*f_growth(z)*hoverh0(a)/real_coverH0;
+		// if(f_chi_ar[i]<1.) {f_chi_ar[i]=0.;}
+		// printf("%lf, %lf, %lf, %lf, %lf \n", f_chi_ar[i],pf,growfac(a),g0,gbias.b1_function(z,ni));
+	}
+}
+
 // Mixture of non-Limber and Limber
 void C_cl_mixed(int L, int LMAX, int ni, int nj, double *Cl, double dev, double tolerance) {
-	// ni = 4;
-	// nj = 4;
+	ni = 4;
+	nj = 4;
 	int i,j,i_block;
 	long l;
 	// run 100 ells at a time, and see if switching to Limber is needed.
@@ -177,19 +195,25 @@ void C_cl_mixed(int L, int LMAX, int ni, int nj, double *Cl, double dev, double 
 	int Nell_block = 100, Nchi = 1000;
 	int ell_ar[Nell_block];
 	double **k1_ar, **k2_ar, **Fk1_ar, **Fk2_ar;
+	double **Fk1_RSD_ar, **Fk2_RSD_ar;
 	// ell_ar = malloc(Nell_block * sizeof(int));
 	k1_ar = malloc(Nell_block * sizeof(double *));
 	k2_ar = malloc(Nell_block * sizeof(double *));
 	Fk1_ar = malloc(Nell_block * sizeof(double *));
 	Fk2_ar = malloc(Nell_block * sizeof(double *));
+	Fk1_RSD_ar = malloc(Nell_block * sizeof(double *));
+	Fk2_RSD_ar = malloc(Nell_block * sizeof(double *));
 	for(i=0;i<Nell_block;i++) {
 		k1_ar[i] = malloc(Nchi * sizeof(double));
 		k2_ar[i] = malloc(Nchi * sizeof(double));
 		Fk1_ar[i] = malloc(Nchi * sizeof(double));
 		Fk2_ar[i] = malloc(Nchi * sizeof(double));
+		Fk1_RSD_ar[i] = malloc(Nchi * sizeof(double));
+		Fk2_RSD_ar[i] = malloc(Nchi * sizeof(double));
 	}
 
 	double chi_ar[Nchi], f1_chi_ar[Nchi], f2_chi_ar[Nchi];
+	double f1_chi_RSD_ar[Nchi], f2_chi_RSD_ar[Nchi];
 	double chi_min = 60., chi_max = 6000.;
 	double dlnchi = log(chi_max/chi_min) / (Nchi - 1.);
 	double dlnk = dlnchi;
@@ -201,28 +225,36 @@ void C_cl_mixed(int L, int LMAX, int ni, int nj, double *Cl, double dev, double 
 	f_chi_for_Psi_cl(chi_ar, Nchi, f1_chi_ar, ni);
 	if(ni != nj) {f_chi_for_Psi_cl(chi_ar, Nchi, f2_chi_ar, nj);}
 
+	f_chi_for_Psi_cl_RSD(chi_ar, Nchi, f1_chi_RSD_ar, ni);
+	if(ni != nj) {f_chi_for_Psi_cl_RSD(chi_ar, Nchi, f2_chi_RSD_ar, nj);}
+
 	// char outfilename[] = "f1_chi4.txt";
+	// char outfilename[] = "f1_chi4_rsd.txt";
 	// FILE *OUT = fopen(outfilename, "w");
 	
 	// for(i=0; i<Nchi; i++) {
-	// 	fprintf(OUT, "%lg %lg", chi_ar[i], f1_chi_ar[i]);
+	// 	// fprintf(OUT, "%lg %lg", chi_ar[i], f1_chi_ar[i]);
+	// 	fprintf(OUT, "%lg %lg", chi_ar[i], f1_chi_RSD_ar[i]);
 	// 	fprintf(OUT, "\n");
 	// }
 	// for(i=0; i<Nchi; i++) {
 	// 	printf("f_chi_ar: %d, %lf\n", i, f1_chi_ar[i]);
 	// }
 	// exit(0);
-	// char outfilename[] = "c_cl4.txt";
-	// FILE *OUT = fopen(outfilename, "w");
+	char outfilename[] = "c_cl4_rsd.txt";
+	FILE *OUT = fopen(outfilename, "w");
 
 
 	i_block = 0;
 	double cl_temp;
 
-	config my_config;
+	config my_config, my_config_RSD;
 	my_config.nu = 1.;
 	my_config.c_window_width = 0.25;
 	my_config.derivative = 0;
+	my_config_RSD.nu = 1.1;
+	my_config_RSD.c_window_width = 0.25;
+	my_config_RSD.derivative = 2;
 
 	double real_coverH0 = cosmology.coverH0 / cosmology.h0;
 	double k1_cH0;
@@ -234,23 +266,28 @@ void C_cl_mixed(int L, int LMAX, int ni, int nj, double *Cl, double dev, double 
 		cfftlog_ells(chi_ar, f1_chi_ar, Nchi, &my_config, ell_ar, Nell_block, k1_ar, Fk1_ar);
 		if(ni != nj) {cfftlog_ells(chi_ar, f2_chi_ar, Nchi, &my_config, ell_ar, Nell_block, k2_ar, Fk2_ar);}
 
+		cfftlog_ells(chi_ar, f1_chi_RSD_ar, Nchi, &my_config_RSD, ell_ar, Nell_block, k1_ar, Fk1_RSD_ar);
+		if(ni != nj) {cfftlog_ells(chi_ar, f2_chi_RSD_ar, Nchi, &my_config_RSD, ell_ar, Nell_block, k2_ar, Fk2_RSD_ar);}
+
 		for(i=0;i<Nell_block;i++) {
 			cl_temp = 0.;
 			for(j=0;j<Nchi;j++) {
 				// printf("k,Fk: %d,%d, %lf,%lf\n", i,j, k1_ar[i][j], Fk1_ar[i][j]);
 				k1_cH0 = k1_ar[i][j] * real_coverH0;
 				if(ni == nj) {
-					cl_temp += (Fk1_ar[i][j])*(Fk1_ar[i][j]) *k1_cH0*k1_cH0*k1_cH0 *p_lin(k1_cH0,1.0)*G_taper(k1_cH0);
+					// cl_temp += (Fk1_ar[i][j])*(Fk1_ar[i][j]) *k1_cH0*k1_cH0*k1_cH0 *p_lin(k1_cH0,1.0)*G_taper(k1_cH0);
+					cl_temp += (Fk1_ar[i][j]+Fk1_RSD_ar[i][j]) * (Fk1_ar[i][j]+Fk1_RSD_ar[i][j]) *k1_cH0*k1_cH0*k1_cH0 *p_lin(k1_cH0,1.0)*G_taper(k1_cH0);
 					// printf("plin,%lg, %lg\n", k1_ar[i][j],p_lin(k1_cH0,1.0));
 				}
 				else {
-					cl_temp += (Fk1_ar[i][j])*(Fk2_ar[i][j]) *k1_cH0*k1_cH0*k1_cH0 *p_lin(k1_cH0,1.0)*G_taper(k1_cH0);
+					// cl_temp += (Fk1_ar[i][j])*(Fk2_ar[i][j]) *k1_cH0*k1_cH0*k1_cH0 *p_lin(k1_cH0,1.0)*G_taper(k1_cH0);
+					cl_temp += (Fk1_ar[i][j]+Fk1_RSD_ar[i][j]) * (Fk2_ar[i][j]+Fk2_RSD_ar[i][j]) *k1_cH0*k1_cH0*k1_cH0 *p_lin(k1_cH0,1.0)*G_taper(k1_cH0);
 				}
 			}
 			Cl[ell_ar[i]] = cl_temp * dlnk * 2./M_PI + C_cl_tomo_nointerp(1.*ell_ar[i],ni,nj) - C_cl_lin_nointerp(1.*ell_ar[i],ni,nj);
 			// printf("cl_t/emp: %d, %lg\n", i, cl_temp);
-			// fprintf(OUT, "%d %lg %lg %lg", ell_ar[i], Cl[ell_ar[i]], C_cl_tomo_nointerp(1.*ell_ar[i],ni,nj), C_cl_lin_nointerp(1.*ell_ar[i],ni,nj));
-			// fprintf(OUT, "\n");
+			fprintf(OUT, "%d %lg %lg %lg", ell_ar[i], Cl[ell_ar[i]], C_cl_tomo_nointerp(1.*ell_ar[i],ni,nj), C_cl_lin_nointerp(1.*ell_ar[i],ni,nj));
+			fprintf(OUT, "\n");
 		}
 		// if(i_block==9) {exit(0);}
 
@@ -266,8 +303,8 @@ void C_cl_mixed(int L, int LMAX, int ni, int nj, double *Cl, double dev, double 
 	for (l = L; l < LMAX; l++){
 		Cl[l]=C_cl_tomo((double)l,ni,nj);
 	}
-	// fclose(OUT);
-	// exit(0);
+	fclose(OUT);
+	exit(0);
 }
 
 double w_tomo_nonLimber(int nt, int ni, int nj){
