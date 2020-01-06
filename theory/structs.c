adf0e0517cd3ef3c4939858a29c4b590a21cd295
Jiachuan Xu <miss199602@163.com>
2020-01-06 11:55:44 -0700

    Updates structure definition in structs.c, to make them compatible with 30 tomographical bins. I add 3 macro at the beginning of structs.c, you can modify the macro to change number of tomographical bins directly next time.

--- theory/structs.c
+++ theory/structs.c
@@ -1,3 +1,8 @@
+#define __NTOMO__ 30
+#define __ZERO_OF_NTOMO__ {0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.}
+#define __ZEROS_OF_NTOMO__ {{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.}}
+#include <string.h>
+
 typedef struct {
   int Ncl;
   int Ntheta;
@@ -68,29 +73,33 @@ typedef struct {
 }cosmopara;
 cosmopara cosmology = {.A_s = 0., .sigma_8=0., .alpha_s =0.0, .M_nu =0., .Omega_nu =0.,.coverH0= 2997.92458, .rho_crit = 7.4775e+21,.MGSigma=0.0,.MGmu=0.0};
 
+// Changes by JX
 typedef struct {
   int shear_Nbin; // number of tomography bins
   int shear_Npowerspectra;// number of tomography power spectra+2+3+...+Nbin
-  double shear_zmax[10]; // code needs modification if more than 10 zbins
-  double shear_zmin[10];
-  double n_source[10];
+  double shear_zmax[__NTOMO__]; // code needs modification if more than 10 zbins
+  double shear_zmin[__NTOMO__];
+  double n_source[__NTOMO__];
   int clustering_Nbin; // number of tomography bins
   int clustering_Npowerspectra;// number of tomography power spectra+2+3+...+Nbin
-  double clustering_zmax[10]; 
-  double clustering_zmin[10];
-  double n_lens[10];
+  double clustering_zmax[__NTOMO__]; 
+  double clustering_zmin[__NTOMO__];
+  double n_lens[__NTOMO__];
   int cluster_Nbin; // number of cluster redshift bins
-  double cluster_zmax[10];
-  double cluster_zmin[10];
+  double cluster_zmax[__NTOMO__];
+  double cluster_zmin[__NTOMO__];
   int cluster_cg_Npowerspectra;// number of cluster-lensing tomography combinations
   int cgl_Npowerspectra;// number of cluster-lensing tomography combinations
   int ggl_Npowerspectra;// number of ggl tomography combinations
   int magnification_Nbin; // number of tomography bins
   int magnification_Npowerspectra;// number of tomography power spectra+2+3+...+Nbin
-  double magnification_zmax[10]; 
-  double magnification_zmin[10];
+  double magnification_zmax[__NTOMO__]; 
+  double magnification_zmin[__NTOMO__];
 }tomopara;
-tomopara tomo = {.n_source = {0.,0.,0.,0.,0.,0.,0.,0.,0.,0.},.n_lens = {0.,0.,0.,0.,0.,0.,0.,0.,0.,0.}};
+tomopara tomo = {
+	.n_source = __ZERO_OF_NTOMO__,
+	.n_lens = __ZERO_OF_NTOMO__
+};
 
 
 typedef struct {
@@ -143,20 +152,26 @@ typedef struct {
 }Cmb;
 Cmb cmb;
 
+// changes by JX
 double bgal_z(double z, int nz);
 typedef  double (*B1_model)(double z, int nz);
 typedef struct{
-  double b[10]; /* linear galaxy bias paramter in clustering bin i*/
-  double b2[10]; /* quadratic bias parameter for redshift bin i */
-  double bs2[10]; /* leading order tidal bias for redshift bin i */
-  double rcorr[10];
-  double hod[10][6]; /*HOD[i] contains HOD parameters of galaxies in clustering bin i, following 5 parameter model of Zehavi et al. 2011 + modification of concentration parameter*/
-  double cg[10];
-  double n_hod[10];
-  double b_mag[10]; /*amplitude of magnification bias, b_mag[i] = 5*s[i]+beta[i] -2 */
+  double b[__NTOMO__]; /* linear galaxy bias paramter in clustering bin i*/
+  double b2[__NTOMO__]; /* quadratic bias parameter for redshift bin i */
+  double bs2[__NTOMO__]; /* leading order tidal bias for redshift bin i */
+  double rcorr[__NTOMO__];
+  double hod[__NTOMO__][6]; /*HOD[i] contains HOD parameters of galaxies in clustering bin i, following 5 parameter model of Zehavi et al. 2011 + modification of concentration parameter*/
+  double cg[__NTOMO__];
+  double n_hod[__NTOMO__];
+  double b_mag[__NTOMO__]; /*amplitude of magnification bias, b_mag[i] = 5*s[i]+beta[i] -2 */
   B1_model b1_function;
 }galpara;
-galpara gbias ={.b2 ={0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0},.bs2 ={0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0},.b1_function = &bgal_z, .b_mag ={0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}}; //default: point to old bgal_z routin
+galpara gbias ={
+	.b2 = __ZERO_OF_NTOMO__,
+	.bs2 = __ZERO_OF_NTOMO__,
+	.b1_function = &bgal_z, 
+	.b_mag = __ZERO_OF_NTOMO__
+}; //default: point to old bgal_z routin
 
 typedef struct{
   double hod[5];
@@ -169,12 +184,13 @@ typedef struct{
 } model_para_redmagic;
 model_para_redmagic redm;
 
+// changes by JX
 typedef struct{
   double N200_min;
   double N200_max;
   int  N200_Nbin;
-  double N_min[10];
-  double N_max[10];
+  double N_min[__NTOMO__];
+  double N_max[__NTOMO__];
   int lbin;
   double l_min;
   double l_max;
@@ -216,8 +232,10 @@ typedef struct { // parameters for power spectrum passed to FASTPT
   cosmopara C;
 }FPTpara;
 FPTpara FPT ={.k_min = 1.e-4, .k_max =1.e+3, .N = 70, .N_per_dec = 10, .N_AB = 7};
+
+// changes by JX
 typedef struct {
-  double A_z[10]; //NLA normalization per source redshift bin, for mpp analyis (activate with like.IA =3)
+  double A_z[__NTOMO__]; //NLA normalization per source redshift bin, for mpp analyis (activate with like.IA =3)
   double A_ia; //A IA see Joachimi2012
   double A2_ia; //placeholder param for quadratic,etc IA
   double beta_ia; //beta IA see Joachimi2012
@@ -225,14 +243,14 @@ typedef struct {
   double eta_ia_highz; //uncertainty in high z evolution
   double oneplusz0_ia; //oneplusz0-ia MegaZ
   double c1rhocrit_ia;
-  double fred[10];
-  double shear_calibration_m[10];
-  double sigma_zphot_shear[10];
-  double bias_zphot_shear[10];
-  double sigma_zphot_clustering[10];
-  double bias_zphot_clustering[10];
-  double sigma_zphot_magnification[10];
-  double bias_zphot_magnification[10];
+  double fred[__NTOMO__];
+  double shear_calibration_m[__NTOMO__];
+  double sigma_zphot_shear[__NTOMO__];
+  double bias_zphot_shear[__NTOMO__];
+  double sigma_zphot_clustering[__NTOMO__];
+  double bias_zphot_clustering[__NTOMO__];
+  double sigma_zphot_magnification[__NTOMO__];
+  double bias_zphot_magnification[__NTOMO__];
   double LF_alpha;
   double LF_P;
   double LF_Q;
@@ -249,27 +267,28 @@ typedef struct {
   double cluster_Mobs_sigma0;
   double cluster_Mobs_sigma_qm;
   double cluster_Mobs_sigma_qz;
-  double cluster_completeness[10];
+  double cluster_completeness[__NTOMO__];
   double cluster_centering_f0;
   double cluster_centering_alpha;
   double cluster_centering_sigma;
   double cluster_centering_M_pivot;
   int N_cluster_MOR;
-  double cluster_MOR[10];
+  double cluster_MOR[__NTOMO__];
   int N_cluster_selection;
-  double cluster_selection[10];
+  double cluster_selection[__NTOMO__];
 }
 nuisancepara;
 nuisancepara nuisance ={.c1rhocrit_ia = 0.0134,
-  .A_z ={0.,0.,0.,0.,0.,0.,0.,0.,0.,0.},
-  .shear_calibration_m = {0.,0.,0.,0.,0.,0.,0.,0.,0.,0.},
-  .sigma_zphot_shear = {0.,0.,0.,0.,0.,0.,0.,0.,0.,0.},
-  .bias_zphot_shear = {0.,0.,0.,0.,0.,0.,0.,0.,0.,0.},
-  .sigma_zphot_clustering = {0.,0.,0.,0.,0.,0.,0.,0.,0.,0.},
-  .bias_zphot_clustering = {0.,0.,0.,0.,0.,0.,0.,0.,0.,0.}};
+  .A_z =,
+  .shear_calibration_m = __ZERO_OF_NTOMO__,
+  .sigma_zphot_shear = __ZERO_OF_NTOMO__,
+  .bias_zphot_shear = __ZERO_OF_NTOMO__,
+  .sigma_zphot_clustering = __ZERO_OF_NTOMO__,
+  .bias_zphot_clustering = __ZERO_OF_NTOMO__
+};
 
 
-
+// changes by JX
 typedef struct { //two parameters for each nuisance parameter: Center (prior.*[0]) + width of Gaussian (prior.*[1])
   double Omega_m;  /* matter density parameter                       */
   double Omega_v;  /* cosmogical constant parameter                  */
@@ -297,13 +316,13 @@ typedef struct { //two parameters for each nuisance parameter: Center (prior.*[0
   double LF_red_alpha[2];
   double LF_red_P[2];
   double LF_red_Q[2];
-  double shear_calibration_m[10][2];
-  double sigma_zphot_shear[10][2];
-  double bias_zphot_shear[10][2];
-  double sigma_zphot_clustering[10][2];
-  double bias_zphot_clustering[10][2];
-  double sigma_zphot_magnification[10][2];
-  double bias_zphot_magnification[10][2];
+  double shear_calibration_m[__NTOMO__][2];
+  double sigma_zphot_shear[__NTOMO__][2];
+  double bias_zphot_shear[__NTOMO__][2];
+  double sigma_zphot_clustering[__NTOMO__][2];
+  double bias_zphot_clustering[__NTOMO__][2];
+  double sigma_zphot_magnification[__NTOMO__][2];
+  double bias_zphot_magnification[__NTOMO__][2];
   double cluster_Mobs_lgM0[2];
   double cluster_Mobs_sigma[2];
   double cluster_Mobs_alpha[2];
@@ -319,11 +338,13 @@ typedef struct { //two parameters for each nuisance parameter: Center (prior.*[0
   double cluster_centering_sigma[2];
   double cluster_centering_M_pivot[2];
 }priorpara;
-priorpara prior = {.shear_calibration_m = {{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.}},
-.sigma_zphot_shear = {{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.}},
-.bias_zphot_shear = {{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.}},
-.sigma_zphot_clustering = {{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.}},
-.bias_zphot_clustering = {{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.},{0.,0.}}};
+priorpara prior = {
+	.shear_calibration_m = __ZEROS_OF_NTOMO__,
+	.sigma_zphot_shear = __ZEROS_OF_NTOMO__,
+	.bias_zphot_shear = __ZEROS_OF_NTOMO__,
+	.sigma_zphot_clustering = __ZEROS_OF_NTOMO__,
+	.bias_zphot_clustering = __ZEROS_OF_NTOMO__
+};
 
 typedef struct{
   double HOD_rm[5][2];
@@ -358,19 +379,21 @@ typedef struct input_cosmo_params {
     double MGmu;
 } input_cosmo_params;
 
+// changes by JX
 typedef struct input_nuisance_params_mpp {
-    double bias[10];
-    double bias2[10];
-    double lens_z_bias[10];
-    double source_z_bias[10];
-    double shear_m[10];
-    double  A_z[10];
-    double  MOR[10];
-    double selection[10];
+    double bias[__NTOMO__];
+    double bias2[__NTOMO__];
+    double lens_z_bias[__NTOMO__];
+    double source_z_bias[__NTOMO__];
+    double shear_m[__NTOMO__];
+    double  A_z[__NTOMO__];
+    double  MOR[__NTOMO__];
+    double selection[__NTOMO__];
 } input_nuisance_params_mpp;
 
+// changes by JX
 typedef struct input_HOD_params {
-    double lgMmin[10];
+    double lgMmin[__NTOMO__];
     double sigma_lgMin;
     double lgM1;
     double lgM0;
@@ -379,13 +402,14 @@ typedef struct input_HOD_params {
     double c_g;
 } input_HOD_params;
 
+// changes by JX
 typedef struct input_nuisance_params {
-    double bias[10];
-    double source_z_bias[10];
+    double bias[__NTOMO__];
+    double source_z_bias[__NTOMO__];
     double source_z_s;
-    double lens_z_bias[10];
+    double lens_z_bias[__NTOMO__];
     double lens_z_s;
-    double shear_m[10];
+    double shear_m[__NTOMO__];
     double A_ia;
     double beta_ia;
     double eta_ia;
@@ -421,6 +445,7 @@ typedef struct {
 } covpar;
 covpar covparams;
 
+// changes by JX
 typedef struct{
    int N_z;
    int N_t;
@@ -430,16 +455,16 @@ typedef struct{
    double k_min; // in h/Mpc
    double k_max; //in h/Mpc
    double f_sky; 
-   double z[10];
-   double V_z[10]; // in (Mpc/h)^3
-   double H_ref[10];
-   double DA_ref[10];
+   double z[__NTOMO__];
+   double V_z[__NTOMO__]; // in (Mpc/h)^3
+   double H_ref[__NTOMO__];
+   double DA_ref[__NTOMO__];
    double** datav;
    double** var;
    double* k;
    double* mu;
-   double n_mt[10][2]; // in (h/Mpc)^3
-   double b_mt[10][2];
+   double n_mt[__NTOMO__][2]; // in (h/Mpc)^3
+   double b_mt[__NTOMO__][2];
    double sigma_z; // fractional accuracy
    double sigma_p; // in km/s
 } GRSpara_mt;
