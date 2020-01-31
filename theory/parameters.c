5cf5627cc889693c4cf49cb455490ae45a227505
xfangcosmo <fang.307@osu.edu>
2020-01-31 16:57:02 -0700

    read fourier cov settings from ini file

--- theory/parameters.c
+++ theory/parameters.c
@@ -79,6 +79,33 @@ void set_cov_parameters_to_(char *covparamfile, int output)
       }
       continue;
     }
+    if(strcmp(name, "lmin")==0)
+    {
+      sscanf(val, "%lf", &covparams.lmin);
+      if(output==1)
+      {
+        printf("lmin %f \n",covparams.lmin);
+      }
+      continue;
+    }
+    else if(strcmp(name, "lmax")==0)
+    {
+      sscanf(val, "%lf", &covparams.lmax);
+      if(output==1)
+      {
+        printf("lmax %f \n",covparams.lmax);
+      }
+      continue;
+    }
+    else if(strcmp(name, "ncl")==0)
+    {
+      sscanf(val, "%d", &covparams.ncl);
+      if(output==1)
+      {
+        printf("Ncl %d \n",covparams.ncl);
+      }
+      continue;
+    }
     else if(strcmp(name, "ng")==0)
     {
       sscanf(val, "%d", &covparams.ng);
