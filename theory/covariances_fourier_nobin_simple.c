b0c161f344b18807aac352e9e89b26fbc1f51189
xfangcosmo <fang.307@osu.edu>
2021-11-21 17:02:17 -0800

    unify NG covariance fourier

--- theory/covariances_fourier_nobin_simple.c
+++ theory/covariances_fourier_nobin_simple.c
@@ -722,34 +722,34 @@ double bin_cov_NG_kk_kk(double l1,double l2, int *z_ar){
 
 int func_for_Cl_Nl(double *Cij, double *Nij, double l, int ni, int nj, int is_lsi, int is_lsj){
   if(is_lsi==1 && is_lsj==1){// case: ll
-    *Cij = C_cl_tomo(l,ni,nj);
+    *Cij = C_cl_tomo_nointerp(l,ni,nj);
     if(ni==nj) {*Nij = 1./(nlens(ni)*survey.n_gal_conversion_factor);}
     return 0;
   }
   if(is_lsi*ls_lsj==-1){ // case: ls, need to determine lens and src bins
-    if(is_lsi==1){*Cij = C_gl_tomo(l,ni,nj);}
-    else{*Cij = C_gl_tomo(l,nj,ni);}
+    if(is_lsi==1){*Cij = C_gl_tomo_nointerp(l,ni,nj);}
+    else{*Cij = C_gl_tomo_nointerp(l,nj,ni);}
     return 0;
   }
   if(is_lsi==-1 && is_lsj==-1){ // case: ss
-    *Cij = C_shear_tomo(l,ni,nj);
+    *Cij = C_shear_tomo_nointerp(l,ni,nj);
     if(ni==nj) {*Nij= pow(survey.sigma_e,2.0)/(2.0*nsource(ni)*survey.n_gal_conversion_factor);}
     return 0;
   }
   if(ni==-1 || nj==-1){ // at least one kcmb field
-    if(is_lsi==1){*Cij = C_gk(l,ni);return 0;}
-    else if(is_lsj==1){*Cij = C_gk(l,nj);return 0;}
-    else if(is_lsi==-1){*Cij = C_ks(l,ni);return 0;}
-    else if(is_lsj==-1){*Cij = C_ks(l,nj);return 0;}
-    else if(ni==-2 || nj==-2){*Cij = C_ky(l);return 0;}
-    else if(ni==-1 && nj==-1){*Cij = C_kk(l); *Nij = kappa_reconstruction_noise(l); return 0;}
+    if(is_lsi==1){*Cij = C_gk_nointerp(l,ni);return 0;}
+    else if(is_lsj==1){*Cij = C_gk_nointerp(l,nj);return 0;}
+    else if(is_lsi==-1){*Cij = C_ks_nointerp(l,ni);return 0;}
+    else if(is_lsj==-1){*Cij = C_ks_nointerp(l,nj);return 0;}
+    else if(ni==-2 || nj==-2){*Cij = C_ky_nointerp(l);return 0;}
+    else if(ni==-1 && nj==-1){*Cij = C_kk_nointerp(l); *Nij = kappa_reconstruction_noise(l); return 0;}
   }
   if(ni==-2 || nj==-2){ // at least one y field, but ky not included
-    if(is_lsi==1){*Cij = C_gy(l,ni);return 0;}
-    else if(is_lsj==1){*Cij = C_gy(l,nj);return 0;}
-    else if(is_lsi==-1){*Cij = C_sy(l,ni);return 0;}
-    else if(is_lsj==-1){*Cij = C_sy(l,nj);return 0;}
-    else if(ni==-2 && nj==-2){*Cij = C_yy(l); *Nij = y_reconstruction_noise(l);return 0;}
+    if(is_lsi==1){*Cij = C_gy_nointerp(l,ni);return 0;}
+    else if(is_lsj==1){*Cij = C_gy_nointerp(l,nj);return 0;}
+    else if(is_lsi==-1){*Cij = C_sy_nointerp(l,ni);return 0;}
+    else if(is_lsj==-1){*Cij = C_sy_nointerp(l,nj);return 0;}
+    else if(ni==-2 && nj==-2){*Cij = C_yy_nointerp(l); *Nij = y_reconstruction_noise(l);return 0;}
   }
   printf("func_for_Cl_Nl: field combination not supported!\n");
   exit(1);
@@ -781,4 +781,103 @@ double cov_G_AB_CD(char ABCD[2][4], double l, double delta_l, int z_ar[4], int i
   }
 
   return ((C13+N13)*(C24+N24)+(C14+N14)*(C23+N23))/(fsky*(2.*l+1.)*delta_l);
+}
+
+
+double tab_cov_NG_AB_CD(char ABCD[2][4], double l1, double l2, int z_ar[4], int is_ls[4]){
+  return 0;
+}
+
+double bgal_a(double a, double nz){
+  return gbias.b1_function(1./a-1.,(int)nz);
+}
+double inner_project_tri_cov_AB_CD(double a,void *params)
+{
+  double k[2],fK,weights,res = 0.;
+  double *ar = (double *) params;
+  int i,j;
+  fK = f_K(chi(a));
+  k[0] = (ar[0]+0.5)/fK;
+  k[1] = (ar[1]+0.5)/fK;
+  
+  weights = dchi_da(a);
+  for(i=2;i<6;i++){
+    if(ar[i]==-1.){
+      weights *= W_k(a,fK);
+    }else if(ar[i]==-2.){
+      weights *= W_y(a,fK);
+      printf("NG cov for y is not yet supported!\n"); exit(1);
+    }else if(is_ls[4+i]==1.){
+      weights *= W_gal(a,ar[i]);
+    }else{
+      weights *= W_kappa(a,fK,ar[i]);
+    }
+  }
+
+  double ssc[2];
+  double sig_b, fsky1, fsky2, fsky_larger;
+  fsky1 = ar[10]; fsky2 = ar[11];
+
+  for(i=0;i<2;i++){
+    ssc[i]=delP_SSC(k[i],a); // no galaxy density field
+    for(j=0;j<2;j++){// if galaxy density field, rescale wrt mean, Eq(A12) in CosmoLike paper
+      if(ar[6+2*i+j]==1){ssc[i] -= bgal_a(a,ar[2+2*i+j])*Pdelta(k[i],a);}
+    }
+  }
+
+  if(fsky1==fsky2){
+    sig_b = survey_variance(a,fsky1);
+  }else{
+    sig_b = cross_survey_variance(a,fsky1,fsky2);
+  }
+  fsky_larger = (fsky1>fsky2 ? fsky1 : fsky2);
+
+  if(weights >0.){
+    if (covparams.cng) {res = tri_matter_cov(k1,k2,a)*pow(fK,-6.)/(fsky_larger*41253.0*survey.area_conversion_factor);}
+    // printf("res cNG:%lg , ", res);
+    res += ssc[0]*ssc[1]*sig_b*pow(fK,-4.); //SSC
+  }
+  // printf("res +SSC, weights:%lg, %lg\n", res, weights);
+  res *= weights;
+  return res;
+}
+
+double cov_NG_AB_CD(char ABCD[2][4], double l1,double l2, int z_ar[4], int is_ls[4]){
+  double amin[2], amax[2], a1,a2,array[12];
+  int zmin, zmax, zlen, zs;
+  int i;
+  int fsky[2];
+
+  fsky_gal = survey.area/41253.0;
+  for(i=0;i<2;i++){
+    if(strcmp(ABCD[i], "ss")==0){
+      zmin = (z_ar[2*i] < z_ar[2*i+1] ? z_ar[2*i] : z_ar[2*i+1]);
+      amin[i] = amin_source(zmin); amax[i] = amax_source(zmin); fsky[i]=fsky_gal;
+    }else if(strcmp(ABCD[i], "ls")==0 || strcmp(ABCD[i], "lk")==0 || strcmp(ABCD[i], "ly")==0){
+      if(is_ls[2*i]==1){zlen = z_ar[2*i];}
+      else{zlen = z_ar[2*i+1];}
+      amin[i] = amin_lens(zlen); a2 = amax_lens(zlen); fsky[i]=fsky_gal;
+    }else if(strcmp(ABCD[i], "ll")==0){
+      zmin = (z_ar[2*i] < z_ar[2*i+1] ? z_ar[2*i] : z_ar[2*i+1]);
+      zmax = (z_ar[2*i] > z_ar[2*i+1] ? z_ar[2*i] : z_ar[2*i+1]);
+      amin[i] = amin_lens(zmax); amax[i] = amax_lens(zmin); fsky[i]=fsky_gal;
+    }else if(strcmp(ABCD[i], "ks")==0){
+      if(is_ls[2*i]==-1){zs = z_ar[2*i];}
+      else{zs = z_ar[2*i+1];}
+      amin[i] = amin_source(zs); amax[i] = amax_source(zs); fsky[i]=fsky_gal;
+    }else if(strcmp(ABCD[i], "kk")==0 || strcmp(ABCD[i], "ky")==0 || strcmp(ABCD[i], "yy")==0){
+      amin[i] = limits.a_min*(1.+1.e-5); amax[i] = 1.-1.e-5; fsky[i]=cmb.fsky;
+    }
+  }
+  a1 = (amin[0]>amin[1] ? amin[0] : amin[1]);
+  a2 = (amax[0]<amax[1] ? amax[0] : amax[1]);
+
+  array[0] = l1;
+  array[1] = l2;
+  for(i=0;i<4;i++){
+    array[2+i] = (double) z_ar[i];
+    array[6+i] = (double) is_ls[i];
+  }
+  for(i=0;i<2;i++){array[10+i] = fsky[i];}
+  return int_gsl_integrate_low_precision(inner_project_tri_cov_AB_CD,(void*)array,a1,a2,NULL,1000);
 }
