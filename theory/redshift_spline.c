e8896be66ad5da4bfb0cb0167f6bdc8df101935a
wolfrayet <yvonne.shenlong@gmail.com>
2025-08-29 14:36:27 -0700

    disable self-defined zdistpar_zmax and zdisrpar_zmin

--- theory/redshift_spline.c
+++ theory/redshift_spline.c
@@ -389,8 +389,8 @@ double zdistr_histo_1(double z, void *params) //return nz(z) based on redshift f
     dz = (z_v[i-1]-z_v[0])/(1.*i -1.);
     zhisto_max=z_v[i-1]+dz;
     zhisto_min=z_v[0];
-    // redshift.shear_zdistrpar_zmin = zhisto_min;
-    // redshift.shear_zdistrpar_zmax = zhisto_max;
+    redshift.shear_zdistrpar_zmin = zhisto_min;
+    redshift.shear_zdistrpar_zmax = zhisto_max;
     free_double_vector(z_v,0,zbins-1);
   }
 
@@ -724,15 +724,11 @@ double pf_histo(double z, void *params) //return pf(z) based on redshift file wi
       }
     }
     fclose(ein);
-    // /* test new setup */
-    // zhisto_max = redshift.clustering_zdistrpar_zmax;
-    // zhisto_min = redshift.clustering_zdistrpar_zmin;
-    // dz = (zhisto_max - zhisto_min) / (double)(i - 1);
     dz = (z_v[i-1]-z_v[0])/(1.*i-1.);
     zhisto_max=z_v[i-1]+dz;
     zhisto_min=z_v[0];
-    // redshift.clustering_zdistrpar_zmin = zhisto_min;
-    // redshift.clustering_zdistrpar_zmax = zhisto_max;
+    redshift.clustering_zdistrpar_zmin = zhisto_min;
+    redshift.clustering_zdistrpar_zmax = zhisto_max;
     free_double_vector(z_v,0,zbins-1);
     if (zhisto_max < tomo.clustering_zmax[tomo.clustering_Nbin-1] || zhisto_min > tomo.clustering_zmin[0]){
       printf("Error in redshift_spline.c:pf_histo.c: %s parameters incompatible with tomo.clustering bin choice\nEXIT!\n",redshift.clustering_REDSHIFT_FILE);
